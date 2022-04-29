b121c69031a90db7086d7a5a16df9ffc04e58d0e
Felipe Ignacio Diaz Alburquenque <felipe.diaz@gmail.com>
2022-04-29 15:35:40 -0400

    :art: [First commit]

---
+++ webpack.config.dev.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dev'),
+    filename: 'app.bundle.js"',
+    publicPath: '/'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+}
