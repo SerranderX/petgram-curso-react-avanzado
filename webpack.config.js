bfa1c3dbcda9777fe0c4fab9f1b894fc8c8e4ea2
Felipe Ignacio Diaz Alburquenque <felipe.diaz@gmail.com>
2022-05-12 11:58:32 -0400

    :sparkles: [ADD] Successfull deploy and config for vercel hosting

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
+  entry: './src/index.js',
   output: {
-    filename: 'app.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
   ]
 }
